7e1755fcfdd32358a623f0c04dd2911c7ae168d9
Marco Marques <gobahmarques@live.com>
2018-03-26 13:59:47 -0300

    [Exclusão Arquivos Desnecessários]

--- admin/enderecos.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$css = 'http://localhost/esportscups/admin/css/';
-	$img = 'http://localhost/esportscups/admin/img/';
-	$js = 'http://localhost/esportscups/admin/js/';
-?>
-
-<base href="http://localhost/esportscups/admin/" />
