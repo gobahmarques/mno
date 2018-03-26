a63dcab8c4b021b0073fd4033f3e44a52bc071b3
Marco Marques <gobahmarques@live.com>
2018-03-26 14:02:15 -0300

    [Exclusão Arquivos Desnecessários]

--- enderecos.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$css = 'http://localhost/esportscups/css/';
-	$img = 'http://localhost/esportscups/img/';
-	$js = 'http://localhost/esportscups/js/';
-?>
-
-<base href="http://localhost/esportscups/" />
